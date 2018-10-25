7948fadcc35f22990b0d78cf132cc6499ebf3787
AlexYinHan <411629507@qq.com>
2018-10-25 22:12:26 +0800

    no output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "List.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    List<int> l;
+    List<int>::iterator it = l.begin();
+    it = l.insert(it, 0);
+    it = l.insert(it, 1);
+    it = l.insert(it, 2);
+    for(it = l.begin(); it != l.end(); it++)
+        cout << *it << endl;
+
 }
